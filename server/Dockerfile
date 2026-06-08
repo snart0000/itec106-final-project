d2a62aa1aa2bf64f06881499c61ba6766acfd645
Albert Tan <alberttan1814@gmail.com>
2026-06-08 19:06:43 +0800

    docker

---
+++ server/Dockerfile
@@ -0,0 +1,7 @@
+FROM php:8.2-apache
+
+COPY . /var/www/html/
+
+RUN docker-php-ext-install pdo pdo_mysql
+
+EXPOSE 80
---
+++ server/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "Hello from PHP!\n";
+echo "PHP Version: " . phpversion() . "\n";
+echo "Current Time: " . date('Y-m-d H:i:s') . "\n";
+?>
